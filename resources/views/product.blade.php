42e7d016419111803873a77170bb6078f0f62334
JellyBool <jellybool@outlook.com>
2019-04-15 00:44:01 +0800

    add videos link support

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,7 +1,7 @@
 <article class="media">
     <figure class="media-left">
         <p class="image is-96x96">
-            <a href="">
+            <a href="https://geixue.com/course/online-payment-service-for-individual-5qc5uy" target="_blank">
                 <img class="is-rounded" src="https://img.geixue.com/image/money.png">
             </a>
         </p>
@@ -9,10 +9,10 @@
     <div class="media-content">
         <div class="content">
             <p>
-                <strong style="font-size: 1.5em"> <a href="">个人收款视频教程</a> </strong>
+                <strong style="font-size: 1.5em"> <a href="https://geixue.com/course/online-payment-service-for-individual-5qc5uy" target="_blank">个人收款视频教程</a> </strong>
                 <small>@JellyBool</small>
                 <br>
-                <a href="" style="color: #4a4a4a;">
+                <a href="https://geixue.com/course/online-payment-service-for-individual-5qc5uy" target="_blank" style="color: #4a4a4a;">
                     本课程主要讲解使用 Payjs 和麦客表单收款这两种方式来实现个人线上收款的功能；
                     包含支付申请的注意事项，支付通知处理，通知安全校验，项目集成等主要内容，看完整个
                     视频课程，您肯定也可以轻松接入个人收款服务！
